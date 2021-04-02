b5964a3d4e71b30ac0ecd2f6d63ac0d897dd7c30
pledgedavis <pledgedavis14@gmail.com>
2021-04-02 15:35:17 -0400

    updating posts controller create/update actions

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -24,29 +24,21 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-
-    respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
+      render json: @post, status: :created, location: @post
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+         render json: @post.errors, status: :unprocessable_entity
+     end
   end
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: "post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    
+   if @post.update(post_params)
+     render json: @content 
+    else
+      render json: @post.errors, status: :unprocessable_entity
+   end
   end
 
   # DELETE /posts/1 or /posts/1.json
